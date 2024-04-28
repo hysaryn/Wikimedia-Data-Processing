608a72de6dc128c9b0dbfa134702d6e8cc484f6d
yolandahe <he.yang7@northeastern.edu>
2024-04-27 22:13:20 -0700

    save wikimedia data into SQL database

--- kafka_consumer_database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
+++ kafka_consumer_database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.entity.WikiMediaData;
+import net.javaguides.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,9 +10,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaDatabaseConsumer {
   private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+  private WikimediaDataRepository wikimediaDataRepository;
+
+  public KafkaDatabaseConsumer(WikimediaDataRepository wikimediaDataRepository) {
+    this.wikimediaDataRepository = wikimediaDataRepository;
+  }
 
   @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
   public void consume(String eventMessage) {
     LOGGER.info(String.format("Event Message received -> %s", eventMessage));
+
+    WikiMediaData wikiMediaData = new WikiMediaData();
+    wikiMediaData.setWikiEventData(eventMessage);
+
+    wikimediaDataRepository.save(wikiMediaData);
   }
+
 }
